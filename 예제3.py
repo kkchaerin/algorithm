2e18c87b0faf2db57e7d9dbd1fd2c8926992fb5d
kkchaerin <48851895+kkchaerin@users.noreply.github.com>
2022-02-06 23:17:53 +0900

    Update 예제3.py
    정답 및 리뷰 추가

--- 예제3.py
+++ 예제3.py
@@ -16,3 +16,22 @@ sumList = list(sumDict.keys())
 sumList.sort(reverse=True)
 print(sumList[k-1])
 
+###### 정답 ######
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+res = set() #set 선언
+
+for i in range(n):
+    for j in range(i+1, n):
+        for m in range(j+1, n):
+            res.add(a[i]+a[j]+a[m])
+            
+res = list(res)
+res.sort(reverse=True)
+    
+
+
+
+###### 리뷰 ######
+#1. set 자료구조 : 중복을 허용하지 x
+#2. set은 sort() 사용이 불가능함. 따라서, 리스트로 변환이 필요.
