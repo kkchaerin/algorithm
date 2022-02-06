ca2a03b06001455dde0a5377c78f2cc2321acaeb
kkchaerin <48851895+kkchaerin@users.noreply.github.com>
2022-02-06 22:52:02 +0900

    Update 예제1.py
    정답 및 리뷰 추가

--- 예제1.py
+++ 예제1.py
@@ -16,3 +16,19 @@ def getKthDivisor(n, k) :
 n, k = map(int, input().split(' '))
 res = getKthDivisor(n, k)
 print(res)
+
+
+
+############### 정답 ###############
+n, k = map(int, input().split())
+cnt = 0
+for i in range(1, n+1):
+    if n%1==0:
+        cnt += 1
+    if cnt==k
+        print(i)
+else:
+    print(-1)
+    
+#### 리뷰
+#파이썬에는 for-else 문법이 존재함.
