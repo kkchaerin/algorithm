ab0979eb1c159f2afc30b536c8aa9e1356d748a4
kkchaerin <48851895+kkchaerin@users.noreply.github.com>
2022-02-06 23:09:35 +0900

    Update 예제2.py
    정답 및 리뷰 추가

--- 예제2.py
+++ 예제2.py
@@ -24,6 +24,22 @@ for i in range(0, T):
 for i in range(0, T):
     print("#"+str(i+1), getKthNumber(paramLists[i], numLists[i]))
     
+    
+    
+###### 정답 ######
+T=int(intput())
+for i in range(T):
+    n, s, e, k = map(int, input().split())
+    a=list(map(int, input().split()))
+    a=a[s-1:e]
+    a.sort()
+    print("#%d %d" %(t, a[k-1]))
+
+    
+###### 리뷰 ######
+#1. sort() 내장 함수 사용!
+#2. 너무 복잡하게 생각함.
+    
 
 
 
